fac1ec2bcf183f568fc0e2addc7fbaf7022587ac
frikksol <frikksol@gmail.com>
2017-10-08 14:13:49 +0200

    Added hall effect sensor to code. Used for triggering relay

--- Receiver/ReceiverController.h
+++ Receiver/ReceiverController.h
@@ -8,12 +8,13 @@ public:
     ReceiverController();
     void setup();
     void loop();
-    void Interrupt();
+    void UpdateMotorPowerServoInterrupt();
+    void HallEffectSensorTriggeredInterrupt();
+    void HallEffectSensorUntriggeredInterrupt();
 
 private:
     const int bluetoothTxPin = 10;
     const int bluetoothRxPin = 11;
-    const int hallEffectSensorPin = 2; //This is also interrupt 0
     const int powerLedPin = 3;
     const int motorPowerLedPin = 4;
     const int motorPowerRelayPin = 5;
@@ -27,7 +28,7 @@ private:
     Servo motorPowerServo;
     volatile int servoPosition = 90;
 
-    const unsigned int hallEffectSensorOnTimeLimit = 1000; //Number of micros the hall effect must be true to trigger
-    unsigned int hallEffectSensorOnTime = 0;
+    const unsigned long hallEffectSensorOnTimeLimit = 500; //Number of micros the hall effect must be true to trigger
+    volatile unsigned long hallEffectSensorOnTime = 0;
     volatile bool motorPowerStatus = false;
 };
