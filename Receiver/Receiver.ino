6e6791a098e755eb94264b35b2bcfdad363e0abf
frikksol <frikksol@gmail.com>
2017-10-21 10:38:15 +0200

    Switched to using HW serial instead of SW serial
    To avoid Interrupt blocking

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,53 +1,34 @@
 #include "ReceiverController.h"
 ReceiverController receiver;
 const int hallEffectSensorPin = 2; //This is also interrupt 0
+const int lolPin = 3;
 
-void setup() {  
-  receiver.setup();
-  SetMotorPowerServoInterrupt();
-  attachInterrupt(digitalPinToInterrupt(hallEffectSensorPin), HallEffectSensorTriggeredInterrupt, FALLING);
+void setup() {
+    receiver.setup();
+    attachInterrupt(digitalPinToInterrupt(hallEffectSensorPin), HallEffectSensorTriggeredInterrupt, FALLING);
+    attachInterrupt(digitalPinToInterrupt(lolPin), lol, FALLING);
+
 }
 
 void loop() {
-  receiver.loop();
+    receiver.UpdateMotorPowerServoInterrupt();
+}
+
+void lol()
+{
+    receiver.ReadNewThrottleValueInterrupt();
+
 }
 
 //HallEffectSensorInterrupt code
 void HallEffectSensorTriggeredInterrupt()
 {
-  receiver.HallEffectSensorTriggeredInterrupt();
-  attachInterrupt(digitalPinToInterrupt(hallEffectSensorPin), HallEffectSensorUntriggeredInterrupt, RISING);
+    receiver.HallEffectSensorTriggeredInterrupt();
+    attachInterrupt(digitalPinToInterrupt(hallEffectSensorPin), HallEffectSensorUntriggeredInterrupt, RISING);
 
 }
 void HallEffectSensorUntriggeredInterrupt()
 {
-  receiver.HallEffectSensorUntriggeredInterrupt();
-  attachInterrupt(digitalPinToInterrupt(hallEffectSensorPin), HallEffectSensorTriggeredInterrupt, FALLING);
+    receiver.HallEffectSensorUntriggeredInterrupt();
+    attachInterrupt(digitalPinToInterrupt(hallEffectSensorPin), HallEffectSensorTriggeredInterrupt, FALLING);
 }
-
-//MotorPowerServoInterrupt code
-void SetMotorPowerServoInterrupt()
-{
-  cli();//stop interrupts
-
-  //set timer2 interrupt at 8kHz
-  TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 1khz increments
-  OCR2A = 249;// = (16*10^6) / (1000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS22 bit for 64 prescaler
-  TCCR2B |= (1 << CS22);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
-  sei();//allow interrupts
-}
-
-ISR(TIMER2_COMPA_vect)
-{//timer2 interrupt 1kHz toggles pin 9
-    receiver.UpdateMotorPowerServoInterrupt();
-}
-  
